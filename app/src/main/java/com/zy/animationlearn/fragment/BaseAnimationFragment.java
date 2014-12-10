662b1db5b741873520828eadda6fe47943965ba2
Simon <zzyy.alex@gmail.com>
2014-12-10 23:04:42 +0800

    12.10

---
+++ app/src/main/java/com/zy/animationlearn/fragment/BaseAnimationFragment.java
@@ -0,0 +1,34 @@
+package com.zy.animationlearn.fragment;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zy.animationlearn.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class BaseAnimationFragment extends Fragment {
+
+
+    public BaseAnimationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_base_animation, container, false);
+    }
+
+
+}
